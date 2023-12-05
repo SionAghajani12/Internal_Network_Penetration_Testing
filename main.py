4b1bdea6f61228aa30140f265bebe0f9aa2e0013
Ghost <101059464+SionAghajani12@users.noreply.github.com>
2023-12-05 18:38:16 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+# main.py
+
 from prettytable import PrettyTable
-from Internal_Network_Scanning import run_nmap_os_scan, run_nmap_service_version_scan, run_nmap_os_scan_pn
-from vulnerability_scanning import run_nikto
+from Internal_Network_Scanning import run_nmap_service_version_scan, run_nmap_os_scan, run_nmap_os_scan_pn
 
 def create_table(header, data):
     table = PrettyTable()
     table.field_names = header
-    table.add_row(data)
+    for row in data:
+        table.add_row(row)
     return str(table)
 
 def main():
@@ -13,34 +15,85 @@ def main():
 
     # Run OS scan Phase 1
     os_scan_result = run_nmap_os_scan(target_ip)
-    os_scan_table = create_table(['OS Scan Result'], [os_scan_result])
+    os_table_header = ['PORT', 'STATE', 'SERVICE']
+    os_table_data = []
 
     print("OS Scan Result:")
-    print(os_scan_table)
+    relevant_lines = False
+
+    for line in os_scan_result.split('\n'):
+        if "PORT" in line and "STATE" in line and "SERVICE" in line:
+            relevant_lines = True
+            continue
+        elif not line.strip() or "unrecognized" in line.lower() or "fingerprint" in line.lower():
+            relevant_lines = False
+
+        if relevant_lines:
+            fields = line.split()
+            if len(fields) >= 3:
+                port = fields[0]
+                state = fields[1]
+                service = fields[2]
+                os_table_data.append([port, state, service])
+
+    os_table = create_table(os_table_header, os_table_data)
+    print(os_table)
     print("-" * 50)
 
     # Run OS scan with -Pn Phase 2
     os_scan_pn_result = run_nmap_os_scan_pn(target_ip)
-    os_scan_pn_table = create_table(['Scan Result with -Pn'], [os_scan_pn_result])
+    os_pn_table_header = ['PORT', 'STATE', 'SERVICE']
+    os_pn_table_data = []
 
     print("Scan Result with -Pn:")
-    print(os_scan_pn_table)
+    relevant_lines = False
+
+    for line in os_scan_pn_result.split('\n'):
+        if "PORT" in line and "STATE" in line and "SERVICE" in line:
+            relevant_lines = True
+            continue
+        elif not line.strip() or "unrecognized" in line.lower() or "fingerprint" in line.lower():
+            relevant_lines = False
+
+        if relevant_lines:
+            fields = line.split()
+            if len(fields) >= 3:
+                port = fields[0]
+                state = fields[1]
+                service = fields[2]
+                os_pn_table_data.append([port, state, service])
+
+    os_pn_table = create_table(os_pn_table_header, os_pn_table_data)
+    print(os_pn_table)
     print("-" * 50)
 
     # Run service version scan Phase 3
     service_version_result = run_nmap_service_version_scan(target_ip)
-    service_version_table = create_table(['Service Version Scan Result'], [service_version_result])
+    service_version_table_header = ['PORT', 'STATE', 'SERVICE', 'VERSION']
+    service_version_table_data = []
 
+    # Extracting relevant information from the service version scan result
+    relevant_lines = False
+
+    for line in service_version_result.split('\n'):
+        if "PORT" in line and "STATE" in line and "SERVICE" in line and "VERSION" in line:
+            relevant_lines = True
+            continue
+        elif not line.strip() or "unrecognized" in line.lower() or "fingerprint" in line.lower():
+            relevant_lines = False
+
+        if relevant_lines:
+            fields = line.split()
+            if len(fields) >= 4:
+                port = fields[0]
+                state = fields[1]
+                service = fields[2]
+                version = ' '.join(fields[3:])
+                service_version_table_data.append([port, state, service, version])
+
+    service_version_table = create_table(service_version_table_header, service_version_table_data)
     print("Service Version Scan Result:")
     print(service_version_table)
-    print("-" * 50)
-
-    # Run Nikto for vulnerability scanning Phase 4
-    nikto_result = run_nikto(target_ip)
-    nikto_table = create_table(['Nikto Scan Result'], [nikto_result])
-
-    print("Nikto Scan Result:")
-    print(nikto_table)
     print("-" * 50)
 
 if __name__ == "__main__":
